0bb2121822b4a4db9efc9e0e0a1ab8880cb8e3cd
0xC00005 <0xC00005@gmail.com>
2020-03-27 05:44:10 -0700

    Review

---
+++ PurdueX/exception_handling.java
@@ -0,0 +1,10 @@
+public class exception_handling {
+        public static void main(String args[]) {
+            try {
+                System.out.print("Hello" + " " + 1 / 0);
+            }
+            catch(ArithmeticException e) {
+        	System.out.print("World");        	
+            }
+        }
+    }
